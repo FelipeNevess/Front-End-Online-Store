bac7704f3eb320c06bbbed9a0aad2283fa2d87f1
FelipeNevess <felipe012neves@gmail.com>
2021-11-28 14:39:02 -0300

    adicionando propTypes ao component Review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Rating } from '@material-ui/lab';
 
 class Review extends Component {
@@ -117,5 +118,9 @@ class Review extends Component {
     );
   }
 }
+
+Review.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default Review;
