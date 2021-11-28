65ba971c7f42ef42657a89f97a9a12d3832bfdb3
FelipeNevess <felipe012neves@gmail.com>
2021-11-28 14:23:11 -0300

    criando components e rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import Routes from './components/routes';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <Routes />
     </div>
   );
 }
